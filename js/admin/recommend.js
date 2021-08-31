7e87ed485c0488bc745f37efa5d2227c1e4a87cb
“HHHSL” <2036802755@qq.com>
2021-08-31 17:27:58 +0800

    博客后台显示效果

---
+++ js/admin/recommend.js
@@ -0,0 +1,39 @@
+document.write("<script language=javascript src='../../api/recommend/recommend.js'></script>");
+$(function () {
+    console.log(getrecommend())
+    InitializedData()
+});
+
+// 初始数据
+function InitializedData() {
+    if(getrecommend() != null && getrecommend().status == 200){
+        var rec = getrecommend()
+        var code =  ''
+        rec.data.forEach(ele => {
+            code +="<tr><th scope='row'>"+ele.id+"</th>"+
+            "<td>"+ele.title+"</td>"+
+            "<td>"+ele.introduce+"</td>"+
+            "<td>"+ele.address+"</td>"+
+            "<td>"+ele.language+"</td><td>"+
+            "<button type='button' class='btn btn-outline-success btn-sm'>修改</button>"+
+            "<button type='button' class='btn btn-outline-danger btn-sm'>删除</button></tr>"
+        });
+    }else{
+        code +="<tr><th scope='row'>null</th>"+
+            "<td>null</td>"+
+            "<td>null</td>"+
+            "<td>null</td>"+
+            "<td>null</td><td>"+
+            "<button type='button' class='btn btn-outline-success btn-sm'>修改</button>"+
+            "<button type='button' class='btn btn-outline-danger btn-sm'>删除</button></tr>"
+        
+        var prompt = "<div id='prompt' class='text-center alert alert-danger alert-dismissible fade show position-absolute top-50 start-50 translate-middle' role='alert'>"+
+        "<strong>Sorry</strong> 服务器出错,请联系管理员"+
+        "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>"
+        $(".admin_rec").append(prompt)
+    }
+    $(".nr_tbody").append(code)
+    setTimeout(function (){
+        $("#prompt").remove()
+    }, 3000);
+}
